64cd995cb8a0a0044154577584a88d3b8328e634
Jake Reardon <jakenbear@gmail.com>
2020-03-31 16:52:17 -0400

    Add Custom Title Option
    Add custom Title option

--- 101 - Scene_Title.rb
+++ 101 - Scene_Title.rb
@@ -1,117 +1,209 @@
 #==============================================================================
-# ** Scene_Title
+# ** Always Sometimes Monsters: Scene_Title
 #------------------------------------------------------------------------------
-#  This class performs the title screen processing.
+#  This is the title screen scene class for ASM
 #==============================================================================
-
+#CUSTOM TITLE SWITCHES
+# ACtivate Custom Title from GO to TItle: Switch 2894
+# Pick from Var : Var 2192
+#
 class Scene_Title < Scene_Base
-  #--------------------------------------------------------------------------
-  # * Start Processing
-  #--------------------------------------------------------------------------
   def start
     super
     SceneManager.clear
     Graphics.freeze
-    create_background
-    create_foreground
-    create_command_window
+		
+    create_backdrop
     play_title_music
-   end
-  #--------------------------------------------------------------------------
-  # * Get Transition Speed
-  #--------------------------------------------------------------------------
-  def transition_speed
-    return 20
+    #FIX FOR F12 DURING DIALOGUE
+    $portrait_window = false
+    portrait = ASM::get_screen.pictures[1]
+    portrait.erase
+    #FIX FOR F12 DURING DIALOGUE
   end
-  #--------------------------------------------------------------------------
-  # * Termination Processing
-  #--------------------------------------------------------------------------
+  
+  def play_title_music
+    if $game_switches[2894] == false
+      $data_system.title_bgm.play
+    else
+      if $game_variables[2192] <= 0
+        RPG::BGM.new("SAMTRAK_Finale", 80, 100).play
+      end
+      if $game_variables[2192] == 1
+        RPG::BGM.new("SAMTRAK_BF07", 80, 100).play
+      end
+      if $game_variables[2192] > 1
+        RPG::BGM.new("SAMTRAK_BF08", 80, 100).play
+      end
+    end
+    RPG::BGS.stop
+    RPG::ME.stop
+  end
+  
   def terminate
     super
-    SceneManager.snapshot_for_background
-    dispose_background
-    dispose_foreground
+		
+    dispose_backdrop
   end
-  #--------------------------------------------------------------------------
-  # * Create Background
-  #--------------------------------------------------------------------------
-  def create_background
-    @sprite1 = Sprite.new
-    @sprite1.bitmap = Cache.title1($data_system.title1_name)
-    @sprite2 = Sprite.new
-    @sprite2.bitmap = Cache.title2($data_system.title2_name)
+  
+  def update
+    super
+		
+    update_backdrop
+    update_credits if @credits_roll
+  end
+  
+  #--------------------------------------------------------------------------#
+  # * Backdrop Functions
+  #--------------------------------------------------------------------------#
+  def create_backdrop
+    # Create Viewports for Backdrop
+    @viewport1 = Viewport.new
+    @viewport1.z = -10
+    @viewport2 = Viewport.new
+    @viewport2.z = 0
+    @viewport3 = Viewport.new
+    @viewport3.z = 6
     
-    center_sprite(@sprite1)
-    center_sprite(@sprite2)
-    #@sprite3 = Sprite.new
-    #@sprite3.bitmap = Cache.title2("F001title")
-    #center_sprite(@sprite3)
+    @viewport1.tone.set(40, 30, 30, 0)
+    @viewport3.tone.set(50, 30, 30, 0)
     
+    # Create Parallax Layers
+    @parallax = []
+    @parallax[0] = ASM_Parallax.new("TitleBackgroundParallax", @viewport1, 0, 0)
+    @parallax[1] = ASM_Parallax.new("TitlePinesBack", @viewport1, 1, 1)
+    @parallax[2] = ASM_Parallax.new("TitlePinesMid", @viewport1, 2, 2)
+    @parallax[3] = ASM_Parallax.new("TitlePinesFront", @viewport1, 3, 3)
+    @parallax[4] = ASM_Parallax.new("TitleForegroundParallax", @viewport1, 6, 4)
+    @parallax[5] = ASM_Parallax.new("TitlePinesTop", @viewport3, 10, 0)
+    
+    # Create Rain Effect
+     #SAM EDIT
+    @weather = Spriteset_Weather.new(@viewport2)
+    @weathervain = 1 + rand(10)
+    
+    if @weathervain == 1
+      @weather.type = :rain
+      @weather.power = 7
+    end
+    
+    if @weathervain == 2
+      @weather.type = :snow
+      @weather.power = 5
+    end
+    
+    if @weathervain > 2
+      @weather.type = :none
+      @weather.power = 7
+    end
   end
-  #--------------------------------------------------------------------------
-  # * Create Foreground
-  #--------------------------------------------------------------------------
-  def create_foreground
-    @foreground_sprite = Sprite.new
-    @foreground_sprite.bitmap = Bitmap.new(Graphics.width, Graphics.height)
-    @foreground_sprite.z = 100
-    draw_game_title if $data_system.opt_draw_title
+  
+  def update_backdrop
+    # Update Parallax scroll
+    if $game_switches[2894] == false
+      @parallax.each do |parallax|
+        parallax.update if !parallax.nil?
+      end
+    end
   end
-  #--------------------------------------------------------------------------
-  # * Draw Game Title
-  #--------------------------------------------------------------------------
-  def draw_game_title
-    @foreground_sprite.bitmap.font.size = 48
-    rect = Rect.new(0, 0, Graphics.width, Graphics.height / 2)
-    @foreground_sprite.bitmap.draw_text(rect, $data_system.game_title, 1)
+  
+  def dispose_backdrop
+    @parallax.each do |parallax|
+      parallax.dispose
+    end
+    
+    @viewport1.dispose
+    @viewport2.dispose
+    @viewport3.dispose
   end
-  #--------------------------------------------------------------------------
-  # * Free Background
-  #--------------------------------------------------------------------------
-  def dispose_background
-    @sprite1.bitmap.dispose
-    @sprite1.dispose
-    @sprite2.bitmap.dispose
-    @sprite2.dispose
-    #@sprite3.bitmap.dispose
-    #@sprite3.dispose
+  
+  #--------------------------------------------------------------------------#
+  # * Layer functions
+  #--------------------------------------------------------------------------#
+  def setup_layers
+    width_scale = ASM::graphics_width_scale
+    height_scale = ASM::graphics_height_scale
+    
+    # Bus Art Layers
+     if $game_switches[2894] == false
+    create_layer(1, "TitleBus", 0, 0, 0, 0)
+    create_layer(2, "BusWheel", 4, 509 * width_scale, 624 * height_scale, 0)
+    create_layer(3, "BusWheel", 4, 552 * width_scale, 624 * height_scale, 0)
+    create_layer(4, "BusWheel", 4, 812 * width_scale, 624 * height_scale, 0)
+    else
+    create_layer(1, "blank_pixel", 0, 0, 0, 0)
+    create_layer(2, "blank_pixel", 4, 509 * width_scale, 624 * height_scale, 0)
+    create_layer(3, "blank_pixel", 4, 552 * width_scale, 624 * height_scale, 0)
+    create_layer(4, "blank_pixel", 4, 812 * width_scale, 624 * height_scale, 0)
+    end
+    
+    # Wheel Rotation
+    get_layer(2).rotate(-150)
+    get_layer(3).rotate(-150)
+    get_layer(4).rotate(-150)
+    
+    # Menu Layers
+    create_layer(5, "SAM_TitleScreen_Logo", 0, 10, 10, 0)
+    create_layer(20, "TitleBackground", 4, Graphics.width / 2, Graphics.height / 2, 255)
   end
-  #--------------------------------------------------------------------------
-  # * Free Foreground
-  #--------------------------------------------------------------------------
-  def dispose_foreground
-    @foreground_sprite.bitmap.dispose
-    @foreground_sprite.dispose
+  
+  #--------------------------------------------------------------------------#
+  # * Animation functions
+  #--------------------------------------------------------------------------#
+	def setup_animations
+		@anim_maxsteps = 2
+	end
+	
+	def skip_animation
+		super
+		
+    get_layer(1).fade(255, 1)
+    get_layer(2).fade(255, 1)
+    get_layer(3).fade(255, 1)
+    get_layer(4).fade(255, 1)
+    get_layer(5).fade(255, 1)
+    get_layer(20).fade(0, 1)
+	end
+	
+	def end_animation
+		super
+		
+		activate_window(0)
+	end
+	
+	def update_animation
+		super
+		
+		case @anim_step
+		when 0
+			get_layer(1).fade(255, 200)
+			get_layer(2).fade(255, 200)
+			get_layer(3).fade(255, 200)
+			get_layer(4).fade(255, 200)
+			get_layer(5).fade(255, 200)
+		when 1
+			get_layer(20).fade(0, 3000)
+		end
+	end
+  
+  #--------------------------------------------------------------------------#
+  # * Window and command functions
+  #--------------------------------------------------------------------------#
+  def setup_windows
+    width_scale = ASM::graphics_width_scale
+    height_scale = ASM::graphics_height_scale
+    
+		window = ASM_Window_TitleCommand.new(20)
+    window.set_handler(:newgame,   method(:command_new_game))
+    window.set_handler(:continue,  method(:command_continue))
+    window.set_handler(:options,   method(:command_options))
+    window.set_handler(:credits,   method(:command_credits))
+    window.set_handler(:quit,      method(:command_shutdown))
+		
+    create_window(0, window, 0)
+    @command_window = window
   end
-  #--------------------------------------------------------------------------
-  # * Move Sprite to Screen Center
-  #--------------------------------------------------------------------------
-  def center_sprite(sprite)
-    sprite.ox = sprite.bitmap.width / 2
-    sprite.oy = sprite.bitmap.height / 2
-    sprite.x = Graphics.width / 2
-    sprite.y = Graphics.height / 2
-  end
-  #--------------------------------------------------------------------------
-  # * Create Command Window
-  #--------------------------------------------------------------------------
-  def create_command_window
-    @command_window = Window_TitleCommand.new
-    @command_window.set_handler(:new_game, method(:command_new_game))
-    @command_window.set_handler(:continue, method(:command_continue))
-    @command_window.set_handler(:shutdown, method(:command_shutdown))
-    #@command_window.set_handler(:shutdown, method(:command_credits))
-    @command_window.y = 48
-    @command_window.x = 348
-    #@command_window.opacity = 0 # JAR - HIDE command window
-  end
-  #--------------------------------------------------------------------------
-  # * Close Command Window
-  #--------------------------------------------------------------------------
-  def close_command_window
-    @command_window.close
-    update until @command_window.close?
-  end
+  
   #--------------------------------------------------------------------------
   # * [New Game] Command
   #--------------------------------------------------------------------------
@@ -121,35 +213,46 @@ class Scene_Title < Scene_Base
     fadeout_all
     $game_map.autoplay
     SceneManager.goto(Scene_Map)
-    #$game_switches[801] = true
+    ASM::Cooking::init_recipe_variables
   end
+  
   #--------------------------------------------------------------------------
-  # * [Continue] Command
+  # * [Options] Command
   #--------------------------------------------------------------------------
-  def command_continue
-    close_command_window
-    SceneManager.call(Scene_Load)
+  def command_options
+		#SceneManager.call(Scene_Options)
+    SceneManager.call(Scene_System)
   end
-  #--------------------------------------------------------------------------
-  # * [Shut Down] Command
-  #--------------------------------------------------------------------------
-  def command_shutdown
-    close_command_window
-    fadeout_all
-    SceneManager.exit
-  end
+  
   #--------------------------------------------------------------------------
   # * [Credits] Command
   #--------------------------------------------------------------------------
   def command_credits
-    SceneManager.call(Scene_Credits)  
+    SceneManager.call(Scene_Credits)
   end
-  #--------------------------------------------------------------------------
-  # * Play Title Screen Music
-  #--------------------------------------------------------------------------
-  def play_title_music
-    $data_system.title_bgm.play
-    RPG::BGS.stop
-    RPG::ME.stop
+  
+  def update_credits
+  end
+end
+
+class Game_CommonEvent
+	alias sam_commonevent_update update
+	def update
+		if SceneManager.scene_is?(Scene_Title)
+			return
+		end
+    
+    sam_commonevent_update
+  end
+end
+
+class Game_Interpreter
+	alias sam_interpreter_update update
+  def update
+    if SceneManager.scene_is?(Scene_Title)
+      Fiber.yield while SceneManager.scene_is?(Scene_Title)
+		else
+      sam_interpreter_update
+    end
   end
 end
